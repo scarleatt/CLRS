ba265b31b9a40a87d36252d5a9127047540fd5fe
resccu <fawnliu2333@gmail.com>
2017-10-29 10:46:21 +0800

    add sort

---
+++ ch01/insertion-sort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[6] = {5,2,4,6,1,3};
+    for (int j = 1; j < 6; j++) {
+        int key = arr[j];
+        int i = j-1;
+        while (i >= 0 && arr[i] > key) {
+            arr[i+1] = arr[i];
+            i--;
+        }
+        arr[i+1] = key;
+        for (i = 0; i < 6; i++)
+            cout<<arr[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
