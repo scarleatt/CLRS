bd456b5a6042baaa85fa183c23a98c2225291c80
resccu <fawnliu2333@gmail.com>
2018-03-03 22:40:02 +0800

    add active bottom_up

--- ch15/1_memoized_cut.cpp
+++ ch15/1_memoized_cut.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int p[11]={0,1,5,8,9,10,17,17,20,24,30};
 vector<int> v;
-int cut(int n) {
+int memorized_cut(int n) {
     if (v[n]>=0) return v[n];
     int q;
     if (n==0) {
@@ -12,15 +12,28 @@ int cut(int n) {
     } else {
         q=INT_MIN;
         for (int i=1; i<=n; i++)
-            q=max(q, p[i]+cut(n-i));
+            q=max(q, p[i]+memorized_cut(n-i));
     }
     v[n]=q;
     return q;
 }
+int bottom_up_cut(int n) {
+    v[0]=0;
+    int q;
+    for (int j=1; j<=n; j++) {
+        q=INT_MIN;
+        for (int i=1; i<=j; i++)
+            q=max(q, p[i]+v[j-i]);
+        v[j]=q;
+    }
+    return v[n];
+}
 int main() {
-    int n=40;
+    int n=30;
     for (int i=0; i<=n; i++)
         v.push_back(INT_MIN);
-    cout<<cut(n)<<endl;
+    cout<<memorized_cut(n)<<endl;
+    v.clear();
+    cout<<bottom_up_cut(n)<<endl;
     return 0;
 }
